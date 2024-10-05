2fdd569094dc3cafde55e89461ca2bd7a689f6cc
Phu Le <phu3262006@gmail.com>
2024-10-04 22:33:42 -0400

    Revert "c"
    This reverts commit bc9914b2c7fbf4535dc288cee2f855d7ad4a49de.

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -1,10 +1,9 @@
 export default function Hero(){
     return(
         <div className="hero">
-            <h3>Hello<span>!</span></h3>
-            <h2>I'm</h2>
-            <h1>Phu Le</h1>
-
+            <h1>Hello<span>!</span></h1>
+            <h2>I'm Phu Le</h2>
+            <h3>Full-Stack Developer.</h3>
         </div>
     
     )
